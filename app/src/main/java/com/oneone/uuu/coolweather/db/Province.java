b695a0ff5ec8082943eab5cc31d49cfb598cb32d
hehai2017 <hehai2017@126.com>
2017-08-15 08:58:16 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/oneone/uuu/coolweather/db/Province.java
@@ -0,0 +1,34 @@
+package com.oneone.uuu.coolweather.db;
+
+/**
+ * Created by uuu on 17-8-14.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public String setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return  provinceCode;
+    }
+
+    public int setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
